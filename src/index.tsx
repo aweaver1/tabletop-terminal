556888034366849aaf329992faf3b6a959f0c39c
Andrew Weaver <agw1190@gmail.com>
2022-08-16 22:10:24 -0700

    Fixing linting

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./report-web-vitals";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './report-web-vitals';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = ReactDOM.createRoot(document.getElementById("root")!);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
